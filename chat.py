41199865075c96b173bd6f5fe8f72880bdad9bea
amakkaraka99 <amakkaraka99@gmail.com>
2023-01-13 18:08:12 +0800

    init

---
+++ chat.py
@@ -0,0 +1,78 @@
+# import modul
+import streamlit as st
+import openai
+from streamlit_chat import message
+# set api key
+openai.api_key = st.secrets["model"]
+
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=["\n", " Human:", " AI:"],
+    )
+    return response.choices[0].text
+
+
+st.title("Chat with AI")
+st.write("This is a chatbot that uses OpenAI's GPT-3 API to generate responses.")
+st.write("You can talk to the bot by typing in the text box and pressing enter.")
+
+# chat
+
+
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text
+    return message
+
+
+st.title("chatBot : Streamlit + openAI")
+
+# Storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+# Chatbot
+# We will get the user's input by calling the get_text function
+
+
+def get_text():
+    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    return input_text
+
+
+user_input = get_text()
+
+if user_input:
+    output = generate_response(user_input)
+    # store the output
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+
+# Displaying the chat
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i],
+                is_user=True, key=str(i) + '_user')
+
+# end of code
